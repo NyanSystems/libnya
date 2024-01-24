78f6e8f2906ab34f9535e65d9ed238e059294794
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-01-24 16:03:01 +0500

    adding all package files

---
+++ default.nix
@@ -0,0 +1,61 @@
+{ lib
+, llvmPackages
+, cmake
+, curl
+, jansson }:
+
+llvmPackages.stdenv.mkDerivation rec {
+  pname = "libnya";
+  version = "0.1.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ curl jansson ];
+
+  cmakeFlags = [
+    "-DENABLE_TESTING=OFF"
+    "-DENABLE_INSTALL=ON"
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/NyanSystems/libnya";
+    description = "Nyan Systems Inc. tomonidan ishlab chiqilgan maxsus kutubxona";
+    licencse = licenses.mit;
+    platforms = with platforms; linux ++ darwin;
+
+    # Bu yerga faqat "Silent Carnage LTC" dasturchilar qo'shilishi mumkin
+    maintainers = [
+      {
+        name = "Sokhibjon Orzikulov";
+        email = "sakhib@orzklv.uz";
+        handle = "orzklv";
+        github = "orzklv";
+        githubId = 54666588;
+        keys = [{
+          fingerprint = "00D2 7BC6 8707 0683 FBB9  137C 3C35 D3AF 0DA1 D6A8";
+        }];
+      }
+      {
+        name = "thelissimus";
+        email = "thelissimus@tuta.io";
+        handle = "thelissimus";
+        github = "thelissimus";
+        githubId = 70096720;
+        keys = [{
+          fingerprint = "6CF0 B081 B4C5 7414 BD7A  09D5 464C A2FF 1D27 D92C";
+        }];
+      }
+      {
+        name = "Otabek Ismoilov";
+        email = "ismoilovdevarchlinux@gmail.com";
+        handle = "ismoilovdevml";
+        github = "ismoilovdevml";
+        githubId = 107093598;
+        keys = [{
+          fingerprint = "2D72 2B1E AEAB 1E28 13F2 A7BA 121E 8D15 C054 757B";
+        }];
+      }
+    ];
+  };
+}
